55a60711a2c127aba6285877f1f503b60174b6a2
Jakhongir Khusanov <worldj95@gmail.com>
2018-11-09 22:32:00 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+// diagram 001-request types explains the whole concept of code below
+// in short it just watches requests trying to access '/' and sends back JSON to whoever made this request  
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+
+// tells Node to listen this port below
+// either we get a port from Heroku environmental variables and handle ports dynamically or just go with 5000 for local development
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);

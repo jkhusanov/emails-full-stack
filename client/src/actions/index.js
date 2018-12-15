ad8654b65dec9def26056a5a3b8fd4ca1d5069de
Jakhongir Khusanov <worldj95@gmail.com>
2018-12-14 23:01:25 -0800

    added fetch user action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
+// dispatch action only when we finish fetch user
+export const fetchUser = () => async dispatch => {
+  const { data } = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: data });
 };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux'; // gives certain components ability to call action creators
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    // materialize css requires a parent div with the class named container
-    <div className="container">
-      {/* BrowserRouter gets only one child ,*/}
-      {/* and path="/" means root route, exact={true} makes sure current url is exactly as path*/}
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      // materialize css requires a parent div with the class named container
+      <div className="container">
+        {/* BrowserRouter gets only one child ,*/}
+        {/* and path="/" means root route, exact={true} makes sure current url is exactly as path*/}
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);

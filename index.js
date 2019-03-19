afc9416350a04978c8ea76368b6789aad2f4d08c
Jakhongir Khusanov <worldj95@gmail.com>
2019-03-18 21:48:10 -0700

    server setup for production build and to handle express.js unkown routes

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const bodyParser = require('body-parser');
 require('./models/User'); // order of this req and below one is important otherwise app will crash
 require('./services/passport');
 
-mongoose.connect(
-  keys.mongoURI,
-  { useNewUrlParser: true }
-);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,7 +19,7 @@ app.use(
     // 30 days in milliseconds
     maxAge: 30 * 24 * 60 * 60 * 1000,
     // key to encrypt a cookie
-    keys: [keys.cookieKey]
+    keys: [keys.cookieKey],
   })
 );
 app.use(passport.initialize());
@@ -33,5 +30,22 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 // tells Node to listen this port below
 // either we get a port from Heroku environment variables and handle ports dynamically or just go with 5000 for local development
+
+// Runs only if we are in production (heroku)
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  // in other words, with will run if all serve routes fail / doesn't exist.
+  // it assumes that React Router (library) know what to do with the incoming route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

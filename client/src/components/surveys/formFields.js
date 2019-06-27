de2c6b91a0d372c7312392dee3ea6b48832681b6
Jahon <worldj95@gmail.com>
2019-06-26 20:54:35 -0700

    Rename survey form fields

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
   { label: 'Survey Title', name: 'title', type: 'text' },
   { label: 'Subject Line', name: 'subject', type: 'text' },
   { label: 'Email Body', name: 'body', type: 'text' },
-  { label: 'Recipient List', name: 'emails', type: 'email' },
+  { label: 'Recipient List', name: 'emails', type: 'recipients' },
 ];

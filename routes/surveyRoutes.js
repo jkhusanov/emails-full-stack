7b657d324a21b179f08c8767a4fc83ebdeb730dc
Jahon <worldj95@gmail.com>
2019-07-07 18:43:46 -0700

    Cleanup survey reponse data

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+/* eslint-disable array-callback-return */
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,7 +16,21 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .unionBy('email', 'surveyId')
+      .value();
+
+    console.log(events);
+
     res.send({});
   });
 
